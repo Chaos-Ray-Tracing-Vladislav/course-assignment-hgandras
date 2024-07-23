16a702681e38f207bdb07ff9a510932a6b689073
hgandras <hgandras@gmail.com>
2024-07-23 21:04:32 +0200

    Acceleration tree works

--- SourceCode/SimpleRayTracer/main.cpp
+++ SourceCode/SimpleRayTracer/main.cpp
@@ -14,43 +14,29 @@
 const std::string PATH = "C:\\Gazsi\\Prog\\ChaosCamp\\RayTracing\\course-assignment-hgandras\\Images";
 const std::string SCENE_PATH = "C:\\Gazsi\\Prog\\ChaosCamp\\RayTracing\\course-assignment-hgandras\\SourceCode\\Scenes\\";
 
-void Task12()
+void Task14()
 {
 	Settings settings;
-	Scene scene0 = Scene::FromFile(SCENE_PATH + "\\Task13\\scene0.crtscene",settings);
+	Scene scene0 = Scene::FromFile(SCENE_PATH + "\\Task14\\scene1.crtscene",settings);
 	std::cout << "Scene read from file" << std::endl;
 	RayTracer renderer0(scene0);
 
 	const auto startBuckets{ std::chrono::high_resolution_clock::now() };
-	renderer0.ParallelBucketsRender(PATH + "\\PPM\\sceneBuckets.ppm");
+	renderer0.ACTreeRender(PATH + "\\PPM\\sceneACTree1.ppm");
 	const auto endBuckets{ std::chrono::high_resolution_clock::now() };
 	long elapsed = std::chrono::duration_cast<std::chrono::seconds>(endBuckets - startBuckets).count();
 
-	std::cout << "Scene0 rendered in parallel buckets in " << elapsed << " seconds" << std::endl;
+	std::cout << "Scene0 rendered in actree in " << elapsed << " seconds" << std::endl;
 
-	const auto startAABB{ std::chrono::high_resolution_clock::now() };
+	/*const auto startAABB{std::chrono::high_resolution_clock::now()};
 	renderer0.AABBRender(PATH + "\\PPM\\sceneAABB.ppm");
 	const auto endAABB{ std::chrono::high_resolution_clock::now() };
 	elapsed = std::chrono::duration_cast<std::chrono::seconds>(endAABB - startAABB).count();
-
-	std::cout << "Scene0 rendered AABB in " << elapsed << " seconds" << std::endl;
-
-	const auto startParallel{ std::chrono::high_resolution_clock::now() };
-	renderer0.ParallelRegionsRender(PATH + "\\PPM\\sceneRegions.ppm");
-	const auto endParallel{ std::chrono::high_resolution_clock::now() };
-	elapsed = std::chrono::duration_cast<std::chrono::seconds>(endParallel - startParallel).count();
-
-	std::cout << "Scene0 rendered in parallel regions in " << elapsed << " seconds" << std::endl;
-
-	const auto start{std::chrono::high_resolution_clock::now()};
-	renderer0.Render(PATH + "\\PPM\\sceneSequential.ppm");
-	const auto end{ std::chrono::high_resolution_clock::now() };
-	elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
-
-	std::cout << "Scene0 rendered in sequential in " << elapsed << " seconds" << std::endl;
+	 
+	std::cout << "Scene0 rendered AABB in " << elapsed << " seconds" << std::endl;*/
 }
 
 
 
-int main(){ Task12(); }
+int main(){ Task14(); }
 
